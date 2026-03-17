d6a6cc15793df1936136c7abca1748c94d72b094
Luis Hurtado <hurtadogarzon@gmail.com>
2026-03-16 19:16:31 -0500

    ajuste reconocimienot de la opcion del numero (modificacion leads.dv) v9

--- leads_db.py
+++ leads_db.py
@@ -7,21 +7,30 @@ def guardar_lead(telefono, interes, presupuesto):
     conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute(
-        """
-        INSERT INTO leads (telefono, nombre, interes, presupuesto, estado)
-        VALUES (%s, %s, %s, %s, %s)
-        """,
-        (
-            telefono,
-            "cliente_whatsapp",
-            interes,
-            presupuesto,
-            "nuevo"
-        )
-    )
+    try:
 
-    conn.commit()
+        cursor.execute(
+            """
+            INSERT INTO leads (telefono, nombre, interes, presupuesto, estado)
+            VALUES (%s, %s, %s, %s, %s)
+            ON CONFLICT (telefono)
+            DO UPDATE SET
+                interes = EXCLUDED.interes,
+                presupuesto = EXCLUDED.presupuesto
+            """,
+            (
+                telefono,
+                "cliente_whatsapp",
+                interes,
+                presupuesto,
+                "nuevo"
+            )
+        )
+
+        conn.commit()
+
+    except Exception as e:
+        print("❌ ERROR GUARDANDO LEAD:", e)
 
     cursor.close()
     conn.close()
